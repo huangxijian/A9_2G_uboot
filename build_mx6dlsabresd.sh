d27525546b06c5b0f1f483b6eafab3baa6f34bde
huangxijian <huangxijian@iot-tw.com>
2019-08-02 15:23:53 +0800

    add all file

---
+++ build_mx6dlsabresd.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6dlsabresd_config 
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
---
+++ burn.sh
@@ -0,0 +1,5 @@
+#!/bin/sh 
+sudo dd if=/dev/zero of=/dev/sdb bs=1k seek=512 conv=fsync count=8 
+sudo dd if=u-boot.imx of=/dev/sdb bs=1k seek=1 conv=fsync
+sync
+
